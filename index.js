cbae4e48580900e6b04c048fa4e78c5bad7cf977
rakibhassannayem <rakibhassannayem@gmail.com>
2025-09-06 16:08:45 +0600

    news rendered

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const categoryContainer = document.getElementById("category-container");
+const newsContainer = document.getElementById("news-container");
 
 const loadCategory = () => {
   fetch("https://news-api-fs.vercel.app/api/categories")
@@ -19,17 +20,40 @@ const displayCategories = (categories) => {
       `;
   });
 
-  categoryContainer.addEventListener('click', (e) =>{
-    if(e.target.localName === 'li'){
-      const allLi = document.querySelectorAll('li')
-      allLi.forEach(li => {
-        li.classList.remove('border-b-4')
+  categoryContainer.addEventListener("click", (e) => {
+    if (e.target.localName === "li") {
+      const allLi = document.querySelectorAll("li");
+      allLi.forEach((li) => {
+        li.classList.remove("border-b-4");
       });
-
-
-      e.target.classList.add('border-b-4')
+      e.target.classList.add("border-b-4");
+      loadNewsByCategory(e.target.id);
     }
-    
-  })
+  });
 };
+
+const loadNewsByCategory = (categoryId) => {
+  fetch(`https://news-api-fs.vercel.app/api/categories/${categoryId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      displayNewsByCategory(data.articles);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const displayNewsByCategory = (articles) => {
+  newsContainer.innerHTML = "";
+  articles.forEach((article) => {
+    newsContainer.innerHTML += `
+    <div class="cursor-pointer">
+      <img src="${article.image.srcset[7].url}">
+      <h1 class="font-bold mt-2">${article.title}</h1>
+      <span class="font-medium text-gray-600">${article.time}</span>
+    </div>
+      `;
+  });
+};
+
 loadCategory();
