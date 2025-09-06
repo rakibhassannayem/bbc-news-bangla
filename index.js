dadeabee1b77f1f0067c1caf083c292273f96466
rakibhassannayem <rakibhassannayem@gmail.com>
2025-09-06 13:54:35 +0600

    li hover design

--- index.js
+++ index.js
@@ -5,14 +5,31 @@ const loadCategory = () => {
     .then((res) => res.json())
     .then((data) => {
       const categories = data.categories;
-      categories.forEach((cat) => {
-        categoryContainer.innerHTML += `
-          <li class="hover:border-b-4 hover:border-red-700 py-3 cursor-pointer">${cat.title}</li>
-        `;
-      });
+      displayCategories(categories);
     })
     .catch((err) => {
       console.log(err);
     });
+};
+
+const displayCategories = (categories) => {
+  categories.forEach((cat) => {
+    categoryContainer.innerHTML += `
+        <li id="${cat.id}" class="hover:border-b-4 border-red-700 hover:border-red-700 py-3 cursor-pointer">${cat.title}</li>
+      `;
+  });
+
+  categoryContainer.addEventListener('click', (e) =>{
+    if(e.target.localName === 'li'){
+      const allLi = document.querySelectorAll('li')
+      allLi.forEach(li => {
+        li.classList.remove('border-b-4')
+      });
+
+
+      e.target.classList.add('border-b-4')
+    }
+    
+  })
 };
 loadCategory();
