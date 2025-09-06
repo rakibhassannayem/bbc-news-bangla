4c5fe4db1037f1c250b1c6998e67f7c46b17946b
rakibhassannayem <rakibhassannayem@gmail.com>
2025-09-06 20:20:53 +0600

    bookmarks handled

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const categoryContainer = document.getElementById("category-container");
 const newsContainer = document.getElementById("news-container");
+const bookmarkContainer = document.getElementById("bookmark-container");
+let bookmarks = [];
+// const deleteBtn = document.getElementById("btn-delete");
 
 const loadCategory = () => {
   fetch("https://news-api-fs.vercel.app/api/categories")
@@ -19,7 +22,7 @@ const displayCategories = (categories) => {
         <li id="${cat.id}" class="hover:border-b-4 border-red-700 hover:border-red-700 py-3 cursor-pointer">${cat.title}</li>
       `;
   });
-
+  categoryContainer.querySelector("li").classList.add("border-b-4");
   categoryContainer.addEventListener("click", (e) => {
     if (e.target.localName === "li") {
       const allLi = document.querySelectorAll("li");
@@ -47,13 +50,53 @@ const displayNewsByCategory = (articles) => {
   newsContainer.innerHTML = "";
   articles.forEach((article) => {
     newsContainer.innerHTML += `
-    <div class="cursor-pointer">
-      <img src="${article.image.srcset[7].url}">
-      <h1 class="font-bold mt-2">${article.title}</h1>
-      <span class="font-medium text-gray-600">${article.time}</span>
+    <div id="${article.id}" class="cursor-pointer border border-gray-200 p-2 shadow-sm rounded-lg flex flex-col justify-between gap-1">
+      <img class="rounded-md" src="${article.image.srcset[7].url}">
+      <h1 class="font-bold">${article.title}</h1>
+      <p class="font-medium text-gray-600">${article.time}</p>
+      <button id="btn-bookmark" class="btn text-red-700  w-fit font-bold border-red-100">Bookmark</button>
     </div>
       `;
   });
 };
 
+newsContainer.addEventListener("click", (e) => {
+  if (e.target.innerText === "Bookmark") {
+    handleBookmarks(e);
+  }
+});
+
+const handleBookmarks = (e) => {
+  const title = e.target.parentNode.children[1].innerText;
+  const id = e.target.parentNode.id;
+  bookmarks.push({
+    title: title,
+    id: id,
+  });
+  displayBookmarks(bookmarks);
+};
+
+const displayBookmarks = (bookmarks) => {
+  bookmarkContainer.innerHTML = "";
+  bookmarks.forEach((bookmark) => {
+    bookmarkContainer.innerHTML += `
+    <div class="border border-gray-300 rounded-lg shadow mt-3 p-2 flex flex-col items-end gap-2">
+      <h1 class="font-bold">${bookmark.title}</h1>
+      <button onclick="handleDeleteBookmarks('${bookmark.id}')" class="btn bg-red-50 text-red-700 "><i class="fa-solid fa-trash-can"></i></button>
+    </div>
+    `;
+  });
+};
+
+const handleDeleteBookmarks = (bookmarkId) =>{
+  const filteredBookmarks = bookmarks.filter(bookmark => bookmark.id !== bookmarkId)
+  bookmarks = filteredBookmarks
+  displayBookmarks(bookmarks)
+}
+
+// deleteBtn.addEventListener('click', (e)=>{
+//   bookmarkContainer.innerHTML = ""
+// })
+
 loadCategory();
+loadNewsByCategory("main");
